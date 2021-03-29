3c8cb95dc1554a24580fe66383195d8cc3aff8e2
jadilovic <adilovic79@yahoo.com>
2021-03-29 15:16:37 +0200

    adding exercise 7

---
+++ ex07/src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+
+import './App.css';
+import Box from "./Box.js";
+
+function App() {
+  return (
+   <div className="container">
+      <Box />
+   </div>
+  );
+}
+
+export default App;
